6ff55f69f6168f5c556dbd0145f04a9866fc2a04
ranieriprg <eddieranieri.prg@gmail.com>
2024-07-03 14:22:00 -0300

    rotas de edicao, dashboard e criar novos pensamentos

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,46 +1,102 @@
-const User = require("../models/User");
+const User = require('../models/User')
 
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs')
 
-module.exports = class AuthController {
+module.exports = class UserController {
   static login(req, res) {
-    res.render("auth/login");
+    res.render('auth/login')
   }
+
+  static async loginPost(req, res) {
+    const { email, password } = req.body
+
+    // find user
+    const user = await User.findOne({ where: { email: email } })
+
+    if (!user) {
+      res.render('auth/login', {
+        message: 'Usuário não encontrado!',
+      })
+
+      return
+    }
+
+    // compare password
+    const passwordMatch = bcrypt.compareSync(password, user.password)
+
+    if (!passwordMatch) {
+      res.render('auth/login', {
+        message: 'Senha inválida!',
+      })
+
+      return
+    }
+
+    // auth user
+    req.session.userid = user.id
+
+    req.flash('message', 'Login realizado com sucesso!')
+
+    req.session.save(() => {
+      res.redirect('/')
+    })
+  }
+
   static register(req, res) {
-    res.render("auth/register");
+    res.render('auth/register')
   }
+
   static async registerPost(req, res) {
-    const { name, email, password, confirmpassword } = req.body;
-    //password match validation
+    const { name, email, password, confirmpassword } = req.body
+
+    // passwords match validation
     if (password != confirmpassword) {
-      req.flash("message", "Senha incorreta, tente novamente!!!");
-      res.render("auth/register");
-      return;
+      req.flash('message', 'As senhas não conferem, tente novamente!')
+      res.render('auth/register')
+
+      return
     }
 
-    //validar usuario
-    const checkIfUserExists = await User.findOne({ where: { email: email } });
+    // email validation
+    const checkIfUserExists = await User.findOne({ where: { email: email } })
+
     if (checkIfUserExists) {
-      req.flash("message", "O e-mail já está em uso.");
-      res.render("auth/register");
-      return;
+      req.flash('message', 'O e-mail já está em uso!')
+      res.render('auth/register')
+
+      return
     }
 
-    //create a password
-    const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(password, salt);
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+
     const user = {
       name,
       email,
       password: hashedPassword,
-    };
-
-    try {
-      await User.create(user);
-      req.flash("message", "Cadastro registrado com sucesso");
-      res.redirect("/");
-    } catch (error) {
-      console.log(`Erro ao salvar usuario, ${error}`);
     }
+
+    User.create(user)
+      .then((user) => {
+        // initialize session
+        req.session.userid = user.id
+
+        // console.log('salvou dado')
+        // console.log(req.session.userid)
+
+        req.session.userid = user.id
+
+        req.flash('message', 'Cadastro realizado com sucesso!')
+
+        req.session.save(() => {
+          res.redirect('/')
+        })
+      })
+      .catch((err) => console.log(err))
   }
-};
+
+  static logout(req, res) {
+    req.session.destroy()
+    res.redirect('/login')
+  }
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const AuthController = require('../controllers/AuthController')
 
-const AuthController = require("../controllers/AuthController");
+router.get('/login', AuthController.login)
+router.post('/login', AuthController.loginPost)
+router.get('/register', AuthController.register)
+router.post('/register', AuthController.registerPost)
+router.get('/logout', AuthController.logout)
 
-router.get("/login", AuthController.login);
-router.get("/register", AuthController.register);
-router.post("/register", AuthController.registerPost);
-
-module.exports = router;
+module.exports = router
